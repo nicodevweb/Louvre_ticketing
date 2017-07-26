226e76694b99f39cd3ae4e9d5222683c8adee8ca
Nicolas <clad87ster@gmail.com>
2017-07-26 15:28:28 +0200

    Updated calendar form as an object in ReservationController, form can now hydrate Reservation object

--- src/Louvre/ReservationBundle/Controller/ReservationController.php
+++ src/Louvre/ReservationBundle/Controller/ReservationController.php
@@ -2,8 +2,12 @@
 
 namespace Louvre\ReservationBundle\Controller;
 
+use Louvre\ReservationBundle\Entity\Reservation;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class ReservationController extends Controller
 {
@@ -12,8 +16,41 @@ class ReservationController extends Controller
 		return $this->render('LouvreReservationBundle:Reservation:index.html.twig');
 	}
 
-	public function calendarAction()
+	public function calendarAction(Request $request)
 	{
-		return $this->render('LouvreReservationBundle:Reservation:calendar.html.twig');
+		// Reservation object is created
+		$reservation = new Reservation();
+
+		// FormBuilder is created by form factory service
+		$form = $this->get('form.factory')->createBuilder(FormType::class, $reservation)
+		// Entity fields wanted are added to the form
+			->add('date',		HiddenType::class)
+			->add('fullDay', 	SubmitType::class, array('label' => 'Journée'))
+			->add('halfDay', 	SubmitType::class, array('label' => 'Demi-journée'))
+			// Form is generated from $formBuilder
+			->getForm()
+		;
+
+		// If POST request executed (if form is submitted)
+		if ($request->isMethod('POST'))
+		{
+			// Now Request <-> Form are related
+			// From now on, $reservation has form values
+			$form->handleRequest($request);
+
+			// Reservation type hydratation
+			$form->get('fullDay')->isClicked() ? $reservation->setType('fullDay') : $reservation->setType('halfDay');
+
+			var_dump($reservation);
+
+			// Reservation object's session is created
+
+			// Redirection to ticket infos' page
+		}
+
+		// Method createView()  is used so the form will be added to view
+		return $this->render('LouvreReservationBundle:Reservation:calendar.html.twig', array(
+			'form' =>$form->createView(),
+		));
 	}
 }
