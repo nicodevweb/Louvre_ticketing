c74d2027056920642234fa8f95367230468aff26
Nicolas <clad87ster@gmail.com>
2017-08-11 18:31:43 +0200

    Updated parameters to set emailing parameters templates and not getting any errors when composer update is done, upgraded app to PHP 7.0.10, installed PHPUnit 6.3.0

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -14,5 +14,5 @@ class DefaultControllerTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
-    }
+    }	
 }
